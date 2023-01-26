fd503334c0b1a5736b077d4fdf051cfc0696a6cc
uTen2c <uten2c@gmail.com>
2023-01-26 16:25:20 +0900

    更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.2")
-    mappings("net.fabricmc:yarn:1.19.2+build.28:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.14.11")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.69.0+1.19.2")
+    minecraft("com.mojang:minecraft:1.19.3")
+    mappings("net.fabricmc:yarn:1.19.3+build.5:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.14.13")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.73.0+1.19.3")
 }
